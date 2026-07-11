a214e7a71606b048fd2b5bb8821337f35b7a99d2
Rumal Medagedara <medagedararrbm@gmail.com>
2026-07-11 13:52:21 +0530

    upload practical files.

---
+++ 09-Bash-Scripting-Essentials/environment.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "===== Environment Variables ====="
+
+echo ""
+
+echo "User : $USER"
+
+echo "Home : $HOME"
+
+echo "Shell : $SHELL"
+
+echo "Current Directory : $PWD"
+
+echo "Hostname : $HOSTNAME"
---
+++ 09-Bash-Scripting-Essentials/my-profile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+name="Rumal"
+
+today=$(date)
+
+currunt_user=$(whoami)
+
+echo "Hello $name"
+echo ""
+echo "Current User : $current_user"
+echo "Today's Date : $today"
